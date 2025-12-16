507abc7ea637dd13d40a225aea5b495046e5fa77
Jappstwr <johnochphil08@gmail.com>
2025-12-16 22:11:12 +0100

    Fredrik MOVES
    he isnt handicaped anymore, no more rullstol. he can move

---
+++ LinkedGame_GameJam/Assets/Jonathan/scripts/RoomWaypoint.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RoomWaypoint : MonoBehaviour
+{
+    [Tooltip("Camera index this waypoint belongs to")]
+    public int roomIndex;
+
+    [Tooltip("Layer name for this room")]
+    public string roomLayer;
+}
